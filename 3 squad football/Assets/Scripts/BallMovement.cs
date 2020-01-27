a3184a3a43ea6c4b5be84f8bd4684a625d92867c
Nicolas Ezequiel Puccio <nico.puccio@hotmail.com>
2020-01-27 17:37:58 -0300

    Began with a little bit of ball behaviour and player movement

---
+++ 3 squad football/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    void Start()
+    {
+        rb.GetComponent<Rigidbody>();
+    }
+    Vector3 vel = new Vector3();
+    // Update is called once per frame
+    void Update()
+    {
+        vel = gameObject.transform.position;
+        if (Input.anyKey)
+        {
+            vel.z = vel.z + 10;
+            rb.velocity = vel;
+            Debug.Log(rb.velocity);
+        }
+    }
+}
---
+++ 3 squad football/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController cc;
+
+    private Vector3 moveDirection = Vector3.zero;
+    public float speed = 10f;
+    
+
+    
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        moveDirection *= speed;
+    }
+
+    private void FixedUpdate()
+    {
+        cc.Move(moveDirection * Time.fixedDeltaTime);
+    }
+}
