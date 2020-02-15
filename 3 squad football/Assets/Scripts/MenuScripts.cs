4ede51c2ddea9dc71d8349b5269fd0e42075af9c
Romibarbosa <romy_jul@hotmail.com>
2020-02-14 23:12:52 -0300

    Added menu, particles

---
+++ 3 squad football/Assets/Scripts/MenuScripts.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScripts : MonoBehaviour
+{
+
+
+   public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void PlayGame(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void HowToPlayScene(string Scene)
+    {
+        SceneManager.LoadScene(Scene);
+    }
+}
--- 3 squad football/Assets/Scripts/Pause.cs
+++ 3 squad football/Assets/Scripts/Pause.cs
@@ -10,7 +10,7 @@ public class Pause : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Pause"))
+        if (Input.GetButtonDown("Pause"))
         {
             PauseGame();
         } 
